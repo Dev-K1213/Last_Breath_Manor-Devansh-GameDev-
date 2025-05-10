9292f0a297d8870d2215c34acf0c596a361a1c04
Dev-K1213 <154006284+Dev-K1213@users.noreply.github.com>
2025-05-09 21:13:01 -0500

    pause and main menu works plus few other tweaks

--- Last_Breath_Manor/Assets/MyStuff/Scripts/StoryController.cs
+++ Last_Breath_Manor/Assets/MyStuff/Scripts/StoryController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StoryController : MonoBehaviour
@@ -8,7 +9,7 @@ public class StoryController : MonoBehaviour
     // Start is called before the first frame update
 
     public Text text;
-    float timer = 7f;
+    float timer = 5f;
 
     int num = 0; 
 
@@ -21,13 +22,36 @@ public class StoryController : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
-        if (timer <= 0.0f){
+        if (timer <= 0.0f && num == 0){
             nextText();
-        }    
+            timer = 5f;
+            num++;
+        }
+
+        if(timer <= 0.0f && num == 1){
+            nextText2();
+            timer = 5f;
+            num++;
+        }
+
+        if(timer <= 0.0f && num == 2){
+            nextText3();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 
     void nextText(){
 
         text.text = "You were getting tired and hungry so you decided to enter...";
+    }
+
+    void nextText2(){
+
+        text.text = "The door closes behind you and locks and now you're trapped...";
+    }
+
+    void nextText3(){
+
+        text.text = "Now you must escape or DIE trying...";
     }
 }
